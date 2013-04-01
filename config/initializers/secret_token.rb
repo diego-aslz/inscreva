3701c6119f176933279ac95659fa1e659a0a7e8c
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-04-01 09:02:21 -0300

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Inscreva::Application.config.secret_token = '1289eef6919c7f7eab67af570e5c3b96b027cb2446c3d1b3782c4e30b93c85086c5368df47d46bf4d730fa02ce0ca57ecff81920433579a1cc5811162a564bbd'
