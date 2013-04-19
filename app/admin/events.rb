e6919a79ee7e9c0ff5714ab6a62652de2d5d603c
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-04-19 16:59:59 -0300

    Configuring active_admin resources for events and users.

--- app/admin/events.rb
+++ app/admin/events.rb
@@ -1,3 +1,15 @@
 ActiveAdmin.register Event do
-  
+  filter :name
+  filter :opens_at
+  filter :closes_at
+
+  index do
+    column :name
+    column :opens_at
+    column :closes_at
+    column :subscriptions do |e|
+      0
+    end
+    default_actions
+  end
 end
--- app/admin/users.rb
+++ app/admin/users.rb
@@ -1,3 +1,11 @@
 ActiveAdmin.register User do
-  
+  filter :email
+  filter :name
+
+  index do
+    column :email
+    column :name
+    column :current_sign_in_ip
+    column :created_at
+  end
 end
