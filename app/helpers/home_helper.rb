2dfab08fc7165459562e94eb1911067f2fc0aff6
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-04-25 11:41:47 -0300

    Replacing simple_form with formtastic.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,5 +1,6 @@
 module HomeHelper
   def link_to_subscribe(event)
-    link_to t(:subscribe), new_subscription_path + '?event_id=' + event.id.to_s
+    link_to t(:subscribe), new_subscription_path + '?event_id=' + event.id.to_s,
+        class: 'btn btn-primary'
   end
 end
