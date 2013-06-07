f834c5dc981b78f7124d46153b743684c41e2581
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-06-07 16:22:25 -0300

    Adicionadas validações de inscrições. Referencia #565.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -2,14 +2,18 @@ class Subscription < ActiveRecord::Base
   belongs_to :event
   belongs_to :user
   attr_accessible :details, :email, :id_card, :event_id
-  validates_presence_of :email, :id_card, :event_id
+  validates_presence_of :email, :id_card, :event_id, :event
   validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
   validate :event_is_ongoing?, if: :event_id
+
+  validates_confirmation_of :email, on: :create, message: I18n.t('helpers.
+      errors.subscription.email.differs_from_confirmation')
+  validates_presence_of :email_confirmation, on: :create
 
   private
 
   def event_is_ongoing?
     errors[:event_id] << I18n.t(
-        'helpers.errors.subscription.event_id.not_ongoing') unless event.ongoing?
+        'helpers.errors.subscription.event_id.not_ongoing') unless event && event.ongoing?
   end
 end
--- spec/models/subscription_spec.rb
+++ spec/models/subscription_spec.rb
@@ -18,6 +18,11 @@ describe Subscription do
         errors_on(:event_id)
   end
 
+  it "validates event exists" do
+    build(:subscription, event_id: -1).should have(1).
+        errors_on(:event_id)
+  end
+
   it "validates email" do
     build(:subscription, email: "anything").should have(1).errors_on(:email)
     build(:subscription, email: "valid_mail@example.com second_mail@example.com").
@@ -27,5 +32,15 @@ describe Subscription do
   it "validates email" do
     build(:subscription, email: "valid_mail@example.com").should have(0).
         errors_on(:email)
+  end
+
+  it "validates email matches its confirmation" do
+    build(:subscription, email: "valid_mail@example.com", email_confirmation: "123").
+        should have(1).errors_on(:email)
+  end
+
+  it "validates presence of email_confirmation" do
+    build(:subscription, email_confirmation: "").should have(1).
+        errors_on(:email_confirmation)
   end
 end
