e65acc8745437f8ba90fa44b8c588866e88c00c8
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-06-13 16:35:34 -0300

    Melhorada tela de exibição de inscrição. Referencia #566.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def text_page_header(text)
+    content_tag(:div, content_tag(:h1, text), class: 'page-header')
+  end
+
+  def icon_tag(icon)
+    content_tag(:i, '', class: icon)
+  end
 end
