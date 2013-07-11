162d40893c344da97802d3d66cf975bc73607218
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-07-11 16:59:27 -0300

    Adicionada geração de número de inscrição.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,7 +5,17 @@ class Subscription < ActiveRecord::Base
   attr_accessible :details, :email, :id_card, :event_id, :name
   serialize :details, Hash
 
+  validates_uniqueness_of :number
+
+  before_create :generate_number
+
   def details_fields
     EventField.where(id: details.keys)
+  end
+
+  private
+
+  def generate_number
+    self.number = Time.zone.now.strftime("%Y%m%d%H%M%S") + sprintf("%03d", rand(999))
   end
 end
