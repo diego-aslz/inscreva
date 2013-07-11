a97844ada14b4abd125b02830cdf814559693444
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-07-11 11:17:45 -0300

    Criado método para salvar arquivos recebidos ao criar inscrição.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -5,7 +5,25 @@ class Subscription < ActiveRecord::Base
   attr_accessible :details, :email, :id_card, :event_id, :name
   serialize :details, Hash
 
+  before_save :save_files
+
+  FILE_STORE = 'public/files'
+
   def details_fields
     EventField.where(id: details.keys)
+  end
+
+  def save_files
+    details_fields.each do |f|
+      if f.type == "file"
+        upload = details[f.id.to_s]
+        if upload
+          file_name = "candidate_#{id}#{File.extname(upload.original_filename)}"
+          dir = File.join FILE_STORE, f.id.to_s
+          details[f.id.to_s] = File.join dir, file_name
+          save_file_data upload, dir, file_name
+        end
+      end
+    end
   end
 end
