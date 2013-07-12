90b509f18f3a7ec7ea690ab2d52c0f26fcb9d54e
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-07-12 14:18:03 -0300

    Submissão de arquivo em funcionamento.

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -7,10 +7,9 @@ class Subscription < ActiveRecord::Base
 
   validates_uniqueness_of :number
 
-  before_create :generate_number
   before_save :save_files
 
-  FILE_STORE = 'public/files'
+  FILE_STORE = '/tmp/files'
 
   def details_fields
     EventField.where(id: details.keys)
@@ -19,17 +18,25 @@ class Subscription < ActiveRecord::Base
   private
 
   def save_files
+    generate_number unless self.number
     details_fields.each do |f|
-      if f.type == "file"
+      if f.field_type == "file"
         upload = details[f.id.to_s]
         if upload
-          dir = File.join FILE_STORE, f.id.to_s
-          file_name = "candidate_#{number}#{File.extname(upload.original_filename)}"
-          details[f.id.to_s] = File.join dir, file_name
-          save_file_data upload, dir, file_name
+          file_name = "subscription_#{number}#{File.extname(upload.original_filename)}"
+          details[f.id.to_s] = file_name
+          save_file_data upload, File.join(FILE_STORE, f.id.to_s), file_name
         end
       end
     end
+  end
+
+  def save_file_data(data, dir, file_name)
+    if data
+      FileUtils.mkdir_p dir
+      File.open(File.join(dir, file_name), "wb") { |f| f.write(data.read) }
+    end
+  end
 
   def generate_number
     self.number = Time.zone.now.strftime("%Y%m%d%H%M%S") + sprintf("%03d", rand(999))
