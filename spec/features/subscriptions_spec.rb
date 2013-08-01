e60ff866685bed4824ddd79ec6451dd54e88f686
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-08-01 18:32:07 -0300

    Correção de i18n em teste.

--- spec/features/subscriptions_spec.rb
+++ spec/features/subscriptions_spec.rb
@@ -6,12 +6,12 @@ feature "Subscriptions" do
     visit root_path
     click_link I18n.t(:subscribe)
 
-    fill_in I18n.t('formtastic.labels.subscription.id_card'), with: '123456'
-    fill_in I18n.t('formtastic.labels.subscription.name'), with: 'Teste'
-    fill_in I18n.t('formtastic.labels.subscription.email'), with: 'teste@teste.com'
-    fill_in I18n.t('formtastic.labels.subscription.email_confirmation'), with: 'teste@teste.com'
-    fill_in I18n.t('formtastic.labels.subscription.password'), with: 'teste@teste.com'
-    fill_in I18n.t('formtastic.labels.subscription.password_confirmation'), with: 'teste@teste.com'
+    fill_in Subscription.human_attribute_name(:id_card), with: '123456'
+    fill_in Subscription.human_attribute_name(:name), with: 'Teste'
+    fill_in Subscription.human_attribute_name(:email), with: 'teste@teste.com'
+    fill_in Subscription.human_attribute_name(:email_confirmation), with: 'teste@teste.com'
+    fill_in Subscription.human_attribute_name(:password), with: 'teste@teste.com', match: :prefer_exact
+    fill_in Subscription.human_attribute_name(:password_confirmation), with: 'teste@teste.com'
 
     click_button I18n.t(:subscribe)
   end
