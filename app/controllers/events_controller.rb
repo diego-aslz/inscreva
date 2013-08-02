87b50590a18cffb01e1b56db15f9f732a1e087d9
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-08-02 17:21:55 -0300

    Correção de listagem e formulário de criação de eventos.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,10 @@
 class EventsController < InheritedResources::Base
   load_and_authorize_resource
+
+  def new
+    @event = Event.new
+    @event.opens_at = Time.zone.now.change(:hour => 0)
+    @event.closes_at = Time.zone.now.change(:hour => 23, :min => 59)
+    new!
+  end
 end
