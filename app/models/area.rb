58f99d947d5e04443814582ed072dc8d47fb69b3
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-08-07 15:14:50 -0300

    Removida entidade Area que não será necessária.

--- app/models/area.rb
+++
@@ -1,5 +0,0 @@
-class Area < ActiveRecord::Base
-  belongs_to :event
-  attr_accessible :name, :requirement, :special_vacation, :vacation, :event_id
-  validates_presence_of :name, :special_vacation, :vacation
-end
