7b07359c847bdc81baef9aba9b27764ed25ebc91
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-08-08 18:02:26 -0300

    Adicionada configuração para deploy com Capistrano, Unicorn e Nginx.

--- Gemfile
+++ Gemfile
@@ -54,18 +54,5 @@ gem 'carrierwave'
 gem 'redcarpet'
 gem 'inherited_resources'
 gem 'angularjs-rails'
-
-# To use ActiveModel has_secure_password
-# gem 'bcrypt-ruby', '~> 3.0.0'
-
-# To use Jbuilder templates for JSON
-# gem 'jbuilder'
-
-# Use unicorn as the app server
-# gem 'unicorn'
-
-# Deploy with Capistrano
-# gem 'capistrano'
-
-# To use debugger
-# gem 'debugger'
+gem 'unicorn'
+gem 'capistrano'
---
+++ config/deploy.rb
@@ -0,0 +1,39 @@
+require "bundler/capistrano"
+
+set :application, "inscreva"
+set :repository,  "https://github.com/nerde/inscreva.git"
+
+set :scm, :git
+# set :git_enable_submodules, 1
+
+set :deploy_to, "/var/www/#{application}"
+set :user, 'root'
+set :use_sudo, false
+set :normalize_asset_timestamps, false
+
+## DEPLOY WHEN SERVER HAS RBENV ###
+set :bundle_flags, "--deployment --quiet --binstubs"
+set :default_environment, {
+  'PATH' => "$HOME/.rbenv/shims:$HOME/.rbenv/bin:$PATH"
+}
+
+role :web, "madeira"                          # Your HTTP server, Apache/etc
+role :app, "madeira"                          # This may be the same as your `Web` server
+role :db,  "madeira", :primary => true # This is where Rails migrations will run
+
+# if you want to clean up old releases on each deploy uncomment this:
+after "deploy:restart", "deploy:cleanup"
+
+namespace :deploy do
+  task :setup_config, roles: :app do
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.yml.example"),
+        "#{shared_path}/config/database.yml"
+  end
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+  after "deploy:finalize_update", "deploy:symlink_config"
+end
