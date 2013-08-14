5f41a09779c8d8d23bf1ab15038699050e1d093a
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-08-14 17:53:22 -0300

    Melhoria de layout, alinhamento à direita de botões de ação e ocultação do menu 'Minhas Inscrições' para usuários sem inscrições.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,14 +23,16 @@ module ApplicationHelper
 
   def minilink_to_edit(model, path, options = {})
     link_to icon_tag('icon-pencil icon-white').html_safe,
-        path, :class => (options[:class] || 'btn btn-mini btn-warning') if can? :edit, model
+        path, :class => (options[:class] || 'btn btn-mini btn-warning'),
+        title: t(:'helpers.links.edit') if can? :edit, model
   end
 
   def minilink_to_destroy(model, options = {})
     link_to icon_tag('icon-trash icon-white').html_safe, model, method: :delete, data: { confirm:
             t(:'helpers.links.confirm', model: (options[:model] ||
                 model.class.model_name.human), value: options[:value]) },
-        class: (options[:class] || 'btn btn-mini btn-danger') if can? :destroy, model
+        class: (options[:class] || 'btn btn-mini btn-danger'),
+        title: t(:'helpers.links.destroy') if can? :destroy, model
   end
 
   def link_to_back(options = {})
--- app/helpers/subscriptions_helper.rb
+++ app/helpers/subscriptions_helper.rb
@@ -1,7 +1,13 @@
 module SubscriptionsHelper
+  def minilink_to_receipt(subscription)
+    link_to icon_tag('icon-print icon-white'), receipt_subscription_path(subscription),
+        target: :_blank, title: t(:print_receipt),
+        class: 'btn btn-mini btn-success' if can?(:receipt, subscription)
+  end
+
   def link_to_receipt(subscription)
     link_to t(:print_receipt), receipt_subscription_path(subscription),
-        target: :_blank
+        target: :_blank, class: 'btn btn-success' if can?(:receipt, subscription)
   end
 
   def show_fill(fill)
