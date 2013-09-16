8522c408304bc9c8a68254ec4562260e8d14b424
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-09-16 08:27:44 -0300

    Adicionando assets do comprovante de inscrição para pré-compilação.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Inscreva::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( ie6.css ie7.css )
+  config.assets.precompile += %w( printing_page.css printing_page.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -64,8 +64,6 @@ Inscreva::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
-
-  config.assets.precompile += %w( ie6.css ie7.css )
 
   # E-mail
   config.action_mailer.default_url_options = {:host => 'yourdomain.com'}
