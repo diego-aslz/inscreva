102893514958bdcdb080cd56a3556f50375faa21
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-09-17 10:20:34 -0300

    Corrigida cópia de campos de eventos para copiar tipos de arquivos permitidos (campo allowed_file_extensions).

--- app/models/event.rb
+++ app/models/event.rb
@@ -51,7 +51,7 @@ class Event < ActiveRecord::Base
     event.fields.each do |field|
       self.fields.build(field.attributes.with_indifferent_access.slice(
           :field_type, :name, :extra, :required, :show_receipt, :group_name,
-          :searchable, :is_numeric, :hint).merge(priority: (p += 1)))
+          :searchable, :is_numeric, :hint, :allowed_file_extensions).merge(priority: (p += 1)))
     end
   end
 end
