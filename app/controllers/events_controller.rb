036c9f7917032410b98b509fc2ebe4253c2b8e4b
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-10-16 10:03:53 -0300

    Correção de problema no controller de eventos. I18n para kaminari.

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -36,13 +36,13 @@ class EventsController < InheritedResources::Base
     return [] if request.get?
     [params.require(:event).permit(:closes_at, :email, :name, :opens_at, :rules_url,
       :technical_email, :identifier, :published, :description,
-      fields_attributes: {
+      fields_attributes: [
         :field_type, :name, :extra, :required, :show_receipt,
         :group_name, :priority, :searchable, :is_numeric, :hint,
         :allowed_file_extensions, :max_file_size
-      },
-      delegations_attributes: {
+      ],
+      delegations_attributes: [
         :user_id, :user_name, :event_id, :role_id
-      })]
+      ])]
   end
 end
