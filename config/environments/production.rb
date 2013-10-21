9baebb599e8caba7a6d8ba9d3b69f55b1b5aa649
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-10-21 15:48:46 -0200

    Compilando assets para impressão de comprovante de inscrição.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -59,7 +59,7 @@ Inscreva::Application.configure do
 
   # Precompile additional assets.
   # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( printing_page.css printing_page.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
