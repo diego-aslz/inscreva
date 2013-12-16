e550f62b2945237464debe6587167998dcacbce1
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2013-12-16 10:48:00 -0200

    Chrome driver.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,6 +6,11 @@ require 'rspec/autorun'
 require 'capybara/rails'
 require 'capybara/rspec'
 
+Capybara.register_driver :chrome do |app|
+  Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
+Capybara.javascript_driver = :chrome
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
