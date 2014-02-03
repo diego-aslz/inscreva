57d93433e1585862ad73ad83e01ba349f89b0503
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2014-02-03 14:48:18 -0200

    Alterada manipulação de erros para renderizar view padrão quando não houver uma específica para o erro.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,11 @@
 class ErrorsController < ApplicationController
   def show
     @exception = env["action_dispatch.exception"]
-    render action: request.path[/\/\d{3}/][1..-1]
+    @status = request.path[/\/\d{3}/][1..-1]
+    if %w(404 422 500).include? @status
+      render action: @status
+    else
+      render action: 'default'
+    end
   end
 end
