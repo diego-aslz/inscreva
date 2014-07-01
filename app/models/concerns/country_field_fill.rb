33370ec81774927c33f3e93a506d975c72494985
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2014-07-01 16:56:39 -0300

    Correção de exibição de campo do tipo país.

--- app/models/concerns/country_field_fill.rb
+++ app/models/concerns/country_field_fill.rb
@@ -2,7 +2,7 @@
 module Concerns
   module CountryFieldFill
     def country_value_to_s
-      value && I18n.t("countries.#{value}") || ''
+      value.blank? && '' || I18n.t("countries.#{value}")
     end
   end
 end
