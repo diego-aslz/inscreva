60b577b549c69721e61772da950db6f8e8a5825c
Diego Aguir Selzlein <diegoselzlein@gmail.com>
2014-07-24 16:25:35 -0300

    Corrigido n+1 no download de arquivos enviados por inscritos.

--- app/services/file_downloader_service.rb
+++ app/services/file_downloader_service.rb
@@ -14,15 +14,16 @@ class FileDownloaderService
     path     = "#{Rails.root}/public/downloads"
     filename = "Inscreva_#{generate_name}.zip"
     zip_name = "#{path}/#{filename}"
+    field_ids = field_ids.map(&:to_i)
 
     Zip::ZipFile.open(zip_name, Zip::ZipFile::CREATE) do |zipfile|
-      @subscriptions.each do |sub|
+      @subscriptions.eager_load(field_fills: :field).where('field_fills.field_id in (?)',
+        field_ids).each do |sub|
         field_ids.each do |field_id|
-          sub.field_fills.where(field_id: field_id).includes(:field).each do |fill|
-            zipfile.add "#{fill.field.name.parameterize}/#{sub.name.parameterize}-" +
-                "#{sub.number}#{File.extname(fill.file_url)}",
-                fill.file_url if !fill.file_url.blank?
-          end
+          fill = sub.field_fills.detect{ |fill| fill.field_id == field_id}
+          zipfile.add "#{fill.field.name.parameterize}/#{sub.name.parameterize}-" +
+              "#{sub.number}#{File.extname(fill.file_url)}",
+              fill.file_url if fill && !fill.file_url.blank?
         end
       end
     end
