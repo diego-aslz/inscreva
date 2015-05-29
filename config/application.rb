d754659c41531c19123ab6671313de5af57f1282
Diego Aguir Selzlein <diego.selzlein@unila.edu.br>
2015-05-29 17:13:00 -0300

    Correção de configuração.

--- config/application.rb
+++ config/application.rb
@@ -3,9 +3,7 @@ require File.expand_path('../boot', __FILE__)
 require 'rails/all'
 require 'csv'
 
-# Require the gems listed in Gemfile, including any gems
-# you've limited to :test, :development, or :production.
-Bundler.require(:default, Rails.env)
+Bundler.require(*Rails.groups)
 
 module Inscreva
   class Application < Rails::Application
@@ -31,11 +29,4 @@ module Inscreva
 
     config.active_record.raise_in_transactional_callbacks = true
   end
-end
-
-if defined?(Bundler)
-  # If you precompile assets before deploying to production, use this line
-  Bundler.require(*Rails.groups(:assets => %w(development test)))
-  # If you want your assets lazily compiled in production, use this line
-  # Bundler.require(:default, :assets, Rails.env)
 end
