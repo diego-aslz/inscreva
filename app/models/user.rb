495a1293a4878860be025c7092a49aa6dd289663
Diego Aguir Selzlein <diego.selzlein@unila.edu.br>
2015-05-29 10:30:32 -0300

    Melhoria de código.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,12 +8,14 @@ class User < ActiveRecord::Base
   has_many :subscriptions, dependent: :destroy
   has_many :delegations, dependent: :destroy
 
-  scope :not_subscribers, -> {
+  def self.not_subscribers
     where('users.id in (select distinct user_id from delegations) or ' +
         'users.id not in (select distinct user_id from subscriptions)')
-  }
-  scope :by_name, ->(name) { where("lower(users.name) like lower(?)",
-    "%#{name}%") }
+  end
+
+  def self.by_name(name)
+    where(arel_table[:name].matches("%#{name}%"))
+  end
 
   def self.search(term)
     term = "%#{term}%"
